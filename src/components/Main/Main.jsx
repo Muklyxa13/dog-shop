6eebeeba11bf33a66f664845971f8c9cb02bc2cf
Muklyxa13 <bloodsmiley13@gmail.com>
2023-02-28 19:29:29 +0300

    fix problems

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,14 @@
 import styles from "./main.module.css"
 import imgDev from "../../images/under_const.png"
-import { motion } from "framer-motion"
 
 export const Main = () => {
   return (
-    <motion.main
-      className={styles.wr}
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1, delay: 0 }}
-    >
+    <main className={styles.wr}>
       <h1 className={styles.title}>В разработке</h1>
       <img className={styles.imgDev} src={imgDev} alt="в разработке" />
       <p className={styles.text}>
         Наш сайт в разработке, но мы готовы к работе!
       </p>
-    </motion.main>
+    </main>
   )
 }
